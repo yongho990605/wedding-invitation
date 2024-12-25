9c5109c1b564959ab3961009aec5b76d10b0a5ef
장용호 <frontmusk@gmail.com>
2024-12-26 01:18:29 +0900

    rules 설정 방식 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,25 +2,22 @@ import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
 export default createConfigForNuxt({
   extends: ['prettier']
+}).overrideRules({
+  'vue/multi-word-component-names': 'off',
+  'vue/block-order': [
+    'error',
+    {
+      order: ['docs', 'template', 'script', 'style', 'i18n:not([locale=en])', 'i18n[locale=en]']
+    }
+  ],
+  'vue/padding-line-between-blocks': ['error'],
+  'import/order': [
+    'error',
+    {
+      groups: ['builtin', 'external', 'internal'],
+      alphabetize: {
+        order: 'asc'
+      }
+    }
+  ]
 })
-  .override('eslint-plugin-vue', {
-    rules: {
-      'vue/multi-word-component-names': 'off',
-      'vue/block-order': [
-        'error',
-        { order: ['docs', 'template', 'script', 'style', 'i18n:not([locale=en])', 'i18n[locale=en]'] }
-      ],
-      'vue/padding-line-between-blocks': ['error']
-    }
-  })
-  .override('eslint-plugin-import', {
-    rules: {
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal'],
-          alphabetize: { order: 'asc' }
-        }
-      ]
-    }
-  })
