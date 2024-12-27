427aaf79efdc20174bd3a2b04fc82b2d4109e7c4
장용호 <frontmusk@gmail.com>
2024-12-27 22:50:00 +0900

    Enhance Tailwind CSS configuration by adding responsive breakpoints for improved layout support

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
+      screens: { 
+        "xs": "320px",
+        "sm": "480px",
+        "md": "768px", 
+        "lg": "1024px",
+        "xl": "1280px",
         "2xl": "1400px",
       },
     },
