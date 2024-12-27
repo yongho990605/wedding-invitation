6104bc96b232303372297c6b69ec9f6c477e2888
장용호 <frontmusk@gmail.com>
2024-12-27 22:26:56 +0900

    Enhance Nuxt.js configuration and add new components support
    - Added components.json for component configuration with Shadcn.
    - Updated nuxt.config.ts to include 'shadcn-nuxt' module and configure component directory.
    - Modified package.json to include new dependencies for Shadcn and Tailwind CSS utilities.
    - Enhanced tailwind.config.js with additional theme customization and animations.
    - Introduced utility functions in lib/utils.ts for class name management.
    - Updated bun.lockb for dependency management.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
 })
