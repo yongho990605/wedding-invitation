369d2fc55113a3f765575c22a574531dee49a0b4
장용호 <frontmusk@gmail.com>
2024-12-28 17:28:26 +0900

    Enhance ESLint configuration and update project dependencies
    - Updated eslint.config.mjs to include new rules for Vue component structure and script indentation.
    - Modified nuxt.config.ts to enable ESLint module and configure standalone settings.
    - Updated package.json to reflect changes in ESLint and related dependencies.
    - Updated bun.lockb for dependency management.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,45 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
-export default createConfigForNuxt({
-  extends: ['prettier']
-}).overrideRules({
-  'vue/multi-word-component-names': 'off',
-  'vue/block-order': [
-    'error',
-    {
-      order: ['docs', 'template', 'script', 'style', 'i18n:not([locale=en])', 'i18n[locale=en]']
-    }
-  ],
-  'vue/padding-line-between-blocks': ['error'],
-  'import/order': [
-    'error',
-    {
-      groups: ['builtin', 'external', 'internal'],
-      alphabetize: {
-        order: 'asc'
+export default await createConfigForNuxt({
+  files: ['**/*.{js,ts,vue}'],
+  extends: ['prettier'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/block-order': [
+      'error',
+      {
+        order: ['template', 'script', 'style']
       }
-    }
-  ]
+    ],
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['template', 'script', 'style']
+      }
+    ],
+    'vue/padding-line-between-blocks': ['error'],
+    'vue/script-indent': [
+      'error',
+      2,
+      {
+        baseIndent: 0,
+        switchCase: 1,
+        ignores: []
+      }
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ]
+  },
+  languageOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser'
+  }
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon', '@nuxt/eslint'],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
+  },
+  eslint: {
+    config: {
+      standalone: true
+    }
   }
 })
