d5e0dc29fe1737e8998132efff06122f5c0a0201
장용호 <frontmusk@gmail.com>
2024-12-28 23:12:21 +0900

    Enhance nuxt.config.ts with app metadata and component directory configuration
    - Added app head configuration including title, meta tags for SEO, and Open Graph properties.
    - Configured component directory settings to streamline component imports.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  app: {
+    head: {
+      title: '장용호 💗 최원비',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '장용호 그리고 최원비, 우리 결혼합니다' },
+        { name: 'og:title', content: '장용호 💗 최원비' },
+        { name: 'og:description', content: '장용호 그리고 최원비, 우리 결혼합니다' },
+        { name: 'og:image', content: '/image1.jpg' }
+      ]
+    }
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
+  components: {
+    dirs: [{ path: '~/components', pathPrefix: false }]
+  },
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
