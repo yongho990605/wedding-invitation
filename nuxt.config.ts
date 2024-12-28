35928318635a6bfe91775ee6c05d518acd4ce44b
장용호 <frontmusk@gmail.com>
2024-12-28 16:35:40 +0900

    Add nuxt icon lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
 
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'shadcn-nuxt'
-  ],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
