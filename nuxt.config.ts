f8c57dcff6ee0f380fba9aa1e4ecc405410356ef
장용호 <frontmusk@gmail.com>
2024-12-28 16:51:35 +0900

    Chore fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
   shadcn: {
     prefix: '',
