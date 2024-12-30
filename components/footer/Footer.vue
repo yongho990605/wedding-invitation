3a25c9344dbf548320e9cd702a231121fd9f2b1b
장용호 <frontmusk@gmail.com>
2024-12-30 15:20:04 +0900

    Footer 컴포넌트 개발

---
+++ components/footer/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer
+    class="relative flex h-[clamp(7rem,20vh,14.2rem)] w-full flex-col items-end overflow-hidden border-t border-[#EEEEEE] bg-white">
+    <p class="font-gyeonggi-batang px-4 pt-7 text-[clamp(0.875rem,2.5vw,1.25rem)] text-[#777777]">
+      Together Love and Gratitude,
+    </p>
+    <div class="absolute -bottom-[clamp(2.3rem,3vw,7rem)] flex w-full items-center justify-center">
+      <p class="font-hakgyoansim-gaeulsopung text-[clamp(4.5rem,10vw,7rem)] font-bold text-[#777777]/15">
+        Yongho&Wonbi
+      </p>
+    </div>
+  </footer>
+</template>
