f54d9ad329610a11f6d2cfe20e5602e458ab25e3
장용호 <frontmusk@gmail.com>
2024-12-30 14:52:40 +0900

    shadcn Carousel 컴포넌트 생성

--- components/ui/card/index.ts
+++ components/ui/card/index.ts
@@ -1,6 +1,14 @@
+import { cva } from 'class-variance-authority'
+
 export { default as Card } from './Card.vue'
 export { default as CardContent } from './CardContent.vue'
 export { default as CardDescription } from './CardDescription.vue'
 export { default as CardFooter } from './CardFooter.vue'
 export { default as CardHeader } from './CardHeader.vue'
 export { default as CardTitle } from './CardTitle.vue'
+
+export const cardVariants = cva('bg-card text-card-foreground rounded-[0.5rem] shadow-sm bg-white flex flex-col', {
+  variants: {
+    outline: { true: 'border' }
+  }
+})
---
+++ components/ui/carousel/CarouselItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    role="group"
+    aria-roledescription="slide"
+    :class="cn('min-w-0 shrink-0 grow-0 basis-full', orientation === 'horizontal' ? 'pl-4' : 'pt-4', props.class)">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+import type { WithClassAsProps } from './interface'
+import { useCarousel } from './useCarousel'
+
+const props = defineProps<WithClassAsProps>()
+
+const { orientation } = useCarousel()
+</script>
