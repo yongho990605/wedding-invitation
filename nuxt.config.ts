063e15df77664fb72e6617899ef5231d37bbc833
장용호 <frontmusk@gmail.com>
2024-12-31 16:14:30 +0900

    test push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,17 +29,9 @@ export default defineNuxtConfig({
     prefix: '',
     componentDir: './components/ui'
   },
-  image: {
+  nitro: {
     prerender: {
-      enabled: false
-    },
-    screens: {
-      xs: 320,
-      sm: 640,
-      md: 768,
-      lg: 1024,
-      xl: 1280,
-      xxl: 1536
+      failOnError: false
     }
   }
 })
