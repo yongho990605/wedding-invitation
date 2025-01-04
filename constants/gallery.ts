1ee37bdb42563e8efe0255ca274be9b10a8ed718
장용호 <frontmusk@gmail.com>
2025-01-04 13:49:25 +0900

    웨딩포토 이미지 constant 파일 생성

---
+++ constants/gallery.ts
@@ -0,0 +1,64 @@
+export const GALLERY_IMAGES = {
+  CONCEPT_1: [
+    '/concept1/image1.jpg',
+    '/concept1/image2.jpg',
+    '/concept1/image3.jpg',
+    '/concept1/image4.jpg',
+    '/concept1/image5.jpg',
+    '/concept1/image6.jpg',
+    '/concept1/image7.jpg',
+    '/concept1/image8.jpg',
+    '/concept1/image9.jpg'
+  ],
+  CONCEPT_2: ['/concept2/image1.jpg', '/concept2/image2.jpg', '/concept2/image3.jpg', '/concept2/image4.jpg'],
+  CONCEPT_3: ['/concept3/image1.jpg', '/concept3/image2.jpg', '/concept3/image3.jpg', '/concept3/image4.jpg'],
+  CONCEPT_4: [
+    '/concept4/image1.jpg',
+    '/concept4/image2.jpg',
+    '/concept4/image3.jpg',
+    '/concept4/image4.jpg',
+    '/concept4/image5.jpg',
+    '/concept4/image6.jpg'
+  ],
+  CONCEPT_5: [
+    '/concept5/image1.jpg',
+    '/concept5/image2.jpg',
+    '/concept5/image3.jpg',
+    '/concept5/image4.jpg',
+    '/concept5/image5.jpg',
+    '/concept5/image6.jpg',
+    '/concept5/image7.jpg',
+    '/concept5/image8.jpg',
+    '/concept5/image9.jpg'
+  ],
+  CONCEPT_6: [
+    '/concept6/image1.jpg',
+    '/concept6/image2.jpg',
+    '/concept6/image3.jpg',
+    '/concept6/image4.jpg',
+    '/concept6/image5.jpg',
+    '/concept6/image6.jpg',
+    '/concept6/image7.jpg'
+  ],
+  CONCEPT_7: [
+    '/concept7/image1.jpg',
+    '/concept7/image2.jpg',
+    '/concept7/image3.jpg',
+    '/concept7/image4.jpg',
+    '/concept7/image5.jpg',
+    '/concept7/image6.jpg',
+    '/concept7/image7.jpg',
+    '/concept7/image8.jpg',
+    '/concept7/image9.jpg',
+    '/concept7/image10.jpg',
+    '/concept7/image11.jpg'
+  ],
+  CONCEPT_8: [
+    '/concept8/image1.jpg',
+    '/concept8/image2.jpg',
+    '/concept8/image3.jpg',
+    '/concept8/image4.jpg',
+    '/concept8/image5.jpg',
+    '/concept8/image6.jpg'
+  ]
+} as const
