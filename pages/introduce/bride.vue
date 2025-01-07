cf4ae796ec53f93ab2b9649c5f73246e19b09249
장용호 <frontmusk@gmail.com>
2025-01-07 23:22:13 +0900

    Enhance bride and groom introduction pages: add background images, profile avatars, and names for improved visual presentation and user experience.

--- pages/introduce/bride.vue
+++ pages/introduce/bride.vue
@@ -1,11 +1,18 @@
 <template>
   <NuxtLayout name="introdution">
-    <div>신부소개</div>
+    <template #background>
+      <img src="/bride/image2.jpg" alt="groom" class="h-full w-full rounded-b object-cover" />
+    </template>
+    <template #profile>
+      <Avatar src="/bride/profile.jpg" size="lg" class="lg:h-24 lg:w-24" />
+    </template>
+    <template #name>
+      <span>신부</span>
+      <span class="text-2xl font-bold">최원비</span>
+    </template>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-definePageMeta({
-  introdution: 'bride'
-})
+definePageMeta({ introdution: 'bride' })
 </script>
--- pages/introduce/groom.vue
+++ pages/introduce/groom.vue
@@ -1,11 +1,18 @@
 <template>
   <NuxtLayout name="introdution">
-    <div>신랑소개</div>
+    <template #background>
+      <img src="/groom/image1.jpg" alt="groom" class="h-full w-full rounded-b object-cover" />
+    </template>
+    <template #profile>
+      <Avatar src="/groom/profile.jpg" size="lg" class="lg:h-24 lg:w-24" />
+    </template>
+    <template #name>
+      <span>신랑</span>
+      <span class="text-2xl font-bold">장용호</span>
+    </template>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-definePageMeta({
-  introdution: 'groom'
-})
+definePageMeta({ introdution: 'groom' })
 </script>
