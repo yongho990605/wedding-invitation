639e57e565cf7e1fe3327761e62b97c44455e3a6
장용호 <frontmusk@gmail.com>
2025-01-11 11:55:21 +0900

    Refactor Gallery component: streamline image rendering by consolidating layout handling and improving class management

--- components/gallery/Gallery.vue
+++ components/gallery/Gallery.vue
@@ -1,42 +1,29 @@
 <template>
   <div :class="galleryVariants({ layout })">
-    <template v-if="layout === 'vertical'">
-      <div v-for="(image, index) in images" :key="image" class="overflow-hidden rounded-lg">
-        <img
-          :src="isDevMode ? image : config.app.baseURL + image"
-          :alt="`Gallery image ${index + 1}`"
-          class="h-full w-full object-contain transition-transform duration-300 hover:scale-105"
-          loading="lazy"
-          oncontextmenu="return false"
-          @dragstart.prevent />
-      </div>
-    </template>
-    <template v-else-if="layout === 'horizontal'">
-      <div
-        v-for="(image, index) in images"
-        :key="image"
-        class="overflow-hidden rounded-lg"
-        :class="index % 2 !== 0 && 'mt-10'">
-        <img
-          :src="image"
-          :alt="`Gallery image ${index + 1}`"
-          class="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
-          loading="lazy"
-          oncontextmenu="return false"
-          @dragstart.prevent />
-      </div>
-    </template>
+    <div
+      v-for="(image, index) in images"
+      :key="image"
+      :class="cn('overflow-hidden rounded-lg', layout === 'horizontal' && index % 2 !== 0 && 'mt-10')">
+      <img
+        :src="isDevMode ? image : config.app.baseURL + image"
+        :alt="`Gallery image ${index + 1}`"
+        class="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
+        loading="lazy"
+        oncontextmenu="return false"
+        @dragstart.prevent />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { cn } from '@/lib/utils'
 import { galleryVariants } from '.'
 import type { GalleryProps } from './types'
-
-const config = useRuntimeConfig()
-const isDevMode = import.meta.dev
 
 withDefaults(defineProps<GalleryProps>(), {
   layout: 'horizontal'
 })
+
+const config = useRuntimeConfig()
+const isDevMode = import.meta.dev
 </script>
