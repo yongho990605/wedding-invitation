f9d08ae1b5ec43dd2a901e478ba67ed5bddeece1
장용호 <frontmusk@gmail.com>
2025-01-12 00:01:52 +0900

    Enhance gallery component layout and functionality
    - Redesigned the gallery layout to improve user interaction with a horizontal scrollable view.
    - Implemented dynamic rendering of gallery images based on the selected category, with responsive adjustments for different concepts.
    - Added a new gallery order array for better localization of gallery item labels.
    - Improved code clarity by utilizing utility functions for class names and managing state with Vue's reactivity.

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    <h1>갤러리</h1>
+  <div class="bg-white">
+    <div
+      class="flex gap-3 overflow-x-auto bg-[#FFFAFB] px-4 [-ms-overflow-style:none] [scrollbar-width:none] lg:gap-4 [&::-webkit-scrollbar]:hidden">
+      <div
+        v-for="(value, key, index) in GALLERY_IMAGES"
+        :key="key"
+        class="flex cursor-pointer flex-col items-center gap-2 py-5"
+        @click="currentGallery = key">
+        <Avatar
+          :src="value[0]"
+          :class="
+            cn(
+              'h-12 w-12 lg:h-14 lg:w-14',
+              currentGallery === key && 'border-2 border-white outline outline-2 outline-[#E58AAB]'
+            )
+          " />
+        <span
+          :class="
+            cn('whitespace-nowrap text-xs text-[#777777] lg:text-sm', currentGallery === key && 'text-[#E58AAB]')
+          ">
+          {{ galleryOrder[index] }}
+        </span>
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center justify-center px-4"
+      :class="GALLERY_IMAGES[currentGallery].length % 2 === 0 ? 'pb-16' : 'pb-6'">
+      <span class="py-5 font-bonitalia text-3xl">gallery</span>
+      <template v-if="currentGallery === 'CONCEPT_5'">
+        <Gallery :images="GALLERY_IMAGES[currentGallery].slice(0, 4)" layout="vertical" class="mb-2.5" />
+        <Gallery :images="GALLERY_IMAGES[currentGallery].slice(4, 9)" />
+      </template>
+      <template v-else>
+        <Gallery
+          :images="GALLERY_IMAGES[currentGallery]"
+          :layout="currentGallery === 'CONCEPT_2' ? 'vertical' : 'horizontal'" />
+      </template>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+import { GALLERY_IMAGES } from '~/constants/gallery'
+
+const galleryOrder = ['첫 번째', '두 번째', '세 번째', '네 번째', '다섯 번째', '여섯 번째', '일곱 번째', '여덟 번째']
+const currentGallery = ref<keyof typeof GALLERY_IMAGES>('CONCEPT_1')
+</script>
