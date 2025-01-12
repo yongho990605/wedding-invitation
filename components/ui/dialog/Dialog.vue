636e631f9495e91fa4e0cd7992c513665eb43e0b
장용호 <frontmusk@gmail.com>
2025-01-12 15:58:53 +0900

    Add dialog components for improved UI interaction

---
+++ components/ui/dialog/Dialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <DialogRoot v-bind="forwarded">
+    <DialogTrigger as-child>
+      <slot />
+    </DialogTrigger>
+    <DialogContent>
+      <slot name="content" />
+    </DialogContent>
+  </DialogRoot>
+</template>
+
+<script setup lang="ts">
+import { DialogRoot, type DialogRootEmits, type DialogRootProps, useForwardPropsEmits } from 'radix-vue'
+
+const props = defineProps<DialogRootProps>()
+const emits = defineEmits<DialogRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
---
+++ components/ui/dialog/DialogContent.vue
@@ -0,0 +1,47 @@
+<template>
+  <DialogPortal>
+    <DialogOverlay
+      class="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+    <DialogContent
+      v-bind="forwarded"
+      :class="
+        cn(
+          'data-[state=open] :slide-in-from-left-1/2 fixed left-1/2 top-0 z-50 w-full min-w-[20rem] -translate-x-1/2 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[50%] data-[state=open]:slide-in-from-top-[50%]',
+          props.class
+        )
+      ">
+      <slot />
+
+      <DialogClose
+        class="absolute right-2 top-2 rounded-sm p-3 opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+        <X class="h-6 w-6 text-white" />
+      </DialogClose>
+    </DialogContent>
+  </DialogPortal>
+</template>
+
+<script setup lang="ts">
+import { X } from 'lucide-vue-next'
+import {
+  DialogClose,
+  DialogContent,
+  type DialogContentEmits,
+  type DialogContentProps,
+  DialogOverlay,
+  DialogPortal,
+  useForwardPropsEmits
+} from 'radix-vue'
+import { computed, type HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<DialogContentProps & { class?: HTMLAttributes['class'] }>()
+const emits = defineEmits<DialogContentEmits>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
