eb58225592bb42fa032995a89d0cff5dc697707d
장용호 <frontmusk@gmail.com>
2025-01-12 16:15:01 +0900

    Update DialogContent.vue to enhance dialog styling and interaction
    - Adjusted dialog height to be full-screen for improved user experience.
    - Added pointer down event prevention to enhance interaction handling.
    - Maintained existing animation and transition effects for consistency.

--- components/ui/dialog/DialogContent.vue
+++ components/ui/dialog/DialogContent.vue
@@ -6,10 +6,11 @@
       v-bind="forwarded"
       :class="
         cn(
-          'data-[state=open] :slide-in-from-left-1/2 fixed left-1/2 top-0 z-50 w-full min-w-[20rem] -translate-x-1/2 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[50%] data-[state=open]:slide-in-from-top-[50%]',
+          'data-[state=open] :slide-in-from-left-1/2 fixed left-1/2 top-0 z-50 h-full w-full min-w-[20rem] -translate-x-1/2 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[50%] data-[state=open]:slide-in-from-top-[50%]',
           props.class
         )
-      ">
+      "
+      @pointer-down-outside="(e) => e.preventDefault()">
       <slot />
 
       <DialogClose
