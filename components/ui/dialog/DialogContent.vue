4705dc6370d38799696027af0f8b6cc55e88057a
장용호 <frontmusk@gmail.com>
2025-01-12 18:25:18 +0900

    Refactor DialogContent.vue to enhance dialog positioning and interaction handling
    - Adjusted dialog positioning to center vertically and horizontally for improved user experience.
    - Replaced pointer down event prevention with close and open auto-focus event prevention for better accessibility.
    - Removed unused DialogClose component to streamline the code while maintaining existing animation and transition effects.

--- components/ui/dialog/DialogContent.vue
+++ components/ui/dialog/DialogContent.vue
@@ -6,27 +6,21 @@
       v-bind="forwarded"
       :class="
         cn(
-          'data-[state=open] :slide-in-from-left-1/2 fixed left-1/2 top-0 z-50 h-full w-full min-w-[20rem] -translate-x-1/2 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[50%] data-[state=open]:slide-in-from-top-[50%]',
+          'data-[state=open] :slide-in-from-left-1/2 fixed left-1/2 top-1/2 z-50 flex h-fit w-full min-w-[20rem] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[50%] data-[state=open]:slide-in-from-top-[50%]',
           props.class
         )
       "
-      @pointer-down-outside="(e) => e.preventDefault()">
+      @close-auto-focus="(e) => e.preventDefault()"
+      @open-auto-focus="(e) => e.preventDefault()">
       <DialogTitle class="hidden" />
       <DialogDescription class="hidden" />
       <slot />
-
-      <DialogClose
-        class="absolute right-2 top-2 rounded-sm p-3 opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <X class="h-6 w-6 text-white" />
-      </DialogClose>
     </DialogContent>
   </DialogPortal>
 </template>
 
 <script setup lang="ts">
-import { X } from 'lucide-vue-next'
 import {
-  DialogClose,
   DialogContent,
   type DialogContentEmits,
   type DialogContentProps,
