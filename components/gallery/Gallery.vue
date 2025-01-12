9e7e04632a92468e84a01503f3fd7f87fba07130
장용호 <frontmusk@gmail.com>
2025-01-12 19:16:23 +0900

    Enhance Gallery component with GalleryDialog integration
    - Added GalleryDialog to manage image display and interaction within the Gallery component.
    - Implemented a slot for custom content within the gallery layout.
    - Updated the structure for improved readability and user experience.
    - Integrated dialog state management for opening images in a dialog view.

--- components/gallery/Gallery.vue
+++ components/gallery/Gallery.vue
@@ -1,28 +1,37 @@
 <template>
-  <div :class="galleryVariants({ layout })">
-    <div
-      v-for="(image, index) in images"
-      :key="image"
-      :class="cn('overflow-hidden rounded-lg', layout === 'horizontal' && index % 2 !== 0 && 'mt-10')">
-      <img
-        :src="isDevMode ? image : config.app.baseURL + image"
-        :alt="`Gallery image ${index + 1}`"
-        class="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
-        loading="lazy"
-        oncontextmenu="return false"
-        @dragstart.prevent />
+  <div>
+    <GalleryDialog v-if="images?.length" v-model:open="dialog.open" :initial-index="dialog.initialIndex" :images />
+    <div :class="galleryVariants({ layout })">
+      <slot>
+        <div
+          v-for="(image, index) in images"
+          :key="image"
+          :class="cn('overflow-hidden rounded-lg', layout === 'horizontal' && index % 2 !== 0 && 'mt-10')"
+          @click="open(index)">
+          <img
+            :src="isDevMode ? image : config.app.baseURL + image"
+            :alt="`Gallery image ${index + 1}`"
+            class="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
+            loading="lazy"
+            oncontextmenu="return false"
+            @dragstart.prevent />
+        </div>
+      </slot>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
+import { useGalleryDialog } from '~/components/gallery/useGalleryDialog'
 import { galleryVariants } from '.'
 import type { GalleryProps } from './types'
 
 withDefaults(defineProps<GalleryProps>(), {
   layout: 'horizontal'
 })
+
+const { dialog, open } = useGalleryDialog()
 
 const config = useRuntimeConfig()
 const isDevMode = import.meta.dev
