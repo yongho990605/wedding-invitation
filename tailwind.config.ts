3e770de074ee0f4f38306e5eada2ac3104f44278
장용호 <frontmusk@gmail.com>
2025-01-14 22:31:21 +0900

    사용하지 않는 폰트 제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ module.exports = {
 
   theme: {
     fontFamily: {
-      'hakgyoansim-kossuyeom': ['Hakgyoansim Kossuyeom', 'sans-serif'],
       pretendard: ['Pretendard', 'sans-serif'],
       'gyeonggi-batang': ['Gyeonggi Batang', 'serif'],
       'hakgyoansim-gaeulsopung': ['Hakgyoansim Gaeulsopung', 'sans-serif'],
