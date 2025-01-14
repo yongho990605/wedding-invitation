4a44a471a2d5b56d86c933779647e0f414479c32
장용호 <frontmusk@gmail.com>
2025-01-14 22:30:36 +0900

    기존 svg, font 제거하고 이미지로 대체

--- components/splash-screen/SplashScreen.vue
+++ components/splash-screen/SplashScreen.vue
@@ -3,17 +3,12 @@
     <div v-if="show" class="fixed inset-0 z-50 flex items-center justify-center bg-[#F1E9E9]">
       <div
         class="min-w-screen-sm absolute left-1/2 top-0 z-50 flex h-full w-full max-w-screen-lg -translate-x-1/2 flex-col items-center justify-center">
-        <Icon name="shared:couple-hand" size="9rem" />
-        <div class="mt-4 flex flex-col items-center font-hakgyoansim-gaeulsopung">
-          <div class="flex justify-center gap-3 text-[#827E7A]">
-            <span class="text-2xl font-bold lg:text-3xl">장용호</span>
-            <span class="mt-3 text-lg font-light lg:text-xl">그리고</span>
-            <span class="text-2xl font-bold lg:text-3xl">최원비</span>
-          </div>
-          <div class="font-hakgyoansim-kossuyeom mt-4 flex justify-center text-3xl text-[#54514E] lg:text-4xl">
-            우리 결혼합니다
-          </div>
-        </div>
+        <img
+          src="/images/shared/splash-screen.png"
+          alt="splash-screen"
+          class="w-[90%]"
+          loading="eager"
+          fetchpriority="high" />
       </div>
     </div>
   </Transition>
