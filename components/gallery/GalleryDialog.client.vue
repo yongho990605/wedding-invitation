85d0276ee3628df58393914295e5378791ae5f53
장용호 <frontmusk@gmail.com>
2025-01-26 22:29:01 +0900

    Refactor gallery dialog layout with improved header and container structure

--- components/gallery/GalleryDialog.client.vue
+++ components/gallery/GalleryDialog.client.vue
@@ -2,29 +2,30 @@
   <Dialog>
     <slot />
     <template #content>
-      <Carousel class="relative flex h-full w-full items-center justify-center" @init-api="setApi">
-        <header
-          class="fixed inset-x-0 top-0 z-10 flex h-14 items-center justify-between bg-gradient-to-b from-black/50 to-transparent px-4">
-          <div class="rounded-lg px-3 py-1 text-white">{{ current }} / {{ totalCount }}</div>
-          <DialogClose
-            class="rounded-lg p-1 opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-            <Icon name="heroicons:x-mark" class="h-6 w-6 text-white" />
-          </DialogClose>
-        </header>
-        <div class="absolute left-0 z-10 flex w-full justify-between px-3">
-          <CarouselPrevious>
-            <Icon name="heroicons:chevron-left" class="h-10 w-10 text-white" />
-          </CarouselPrevious>
-          <CarouselNext>
-            <Icon name="heroicons:chevron-right" class="h-10 w-10 text-white" />
-          </CarouselNext>
-        </div>
-        <CarouselContent class="flex w-full gap-40 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
-          <CarouselItem v-for="(image, i) in images" :key="i" class="shrink-0 grow-0 basis-full">
-            <img :src="withDomain(image)" class="object-cover" />
-          </CarouselItem>
-        </CarouselContent>
-      </Carousel>
+      <div class="relative h-full w-full">
+        <Carousel class="flex h-full w-full items-center justify-center" @init-api="setApi">
+          <div class="absolute left-0 right-0 top-4 z-10 flex justify-between px-4">
+            <div class="rounded-lg bg-black/50 px-3 py-1 text-white">{{ current }} / {{ totalCount }}</div>
+            <DialogClose
+              class="rounded-lg bg-black/50 p-1 opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+              <Icon name="heroicons:x-mark" class="h-6 w-6 text-white" />
+            </DialogClose>
+          </div>
+          <div class="absolute left-0 z-10 flex w-full justify-between px-3">
+            <CarouselPrevious>
+              <Icon name="heroicons:chevron-left" class="h-10 w-10 text-white" />
+            </CarouselPrevious>
+            <CarouselNext>
+              <Icon name="heroicons:chevron-right" class="h-10 w-10 text-white" />
+            </CarouselNext>
+          </div>
+          <CarouselContent class="flex w-full gap-40 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
+            <CarouselItem v-for="(image, i) in images" :key="i" class="shrink-0 grow-0 basis-full">
+              <img :src="withDomain(image)" class="object-cover" />
+            </CarouselItem>
+          </CarouselContent>
+        </Carousel>
+      </div>
     </template>
   </Dialog>
 </template>
