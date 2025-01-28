5100cf53e308adab408d6e5308b3558cdcd08d9b
장용호 <frontmusk@gmail.com>
2025-01-28 21:10:33 +0900

    Simplify AccordionTrigger component structure

--- components/ui/accordion/AccordionTrigger.vue
+++ components/ui/accordion/AccordionTrigger.vue
@@ -1,21 +1,19 @@
 <template>
-  <AccordionHeader class="flex">
-    <AccordionTrigger
-      v-bind="delegatedProps"
-      :class="
-        cn('flex flex-1 items-center justify-center transition-all [&[data-state=open]>svg]:rotate-180', props.class)
-      ">
-      <slot />
-      <slot name="icon">
-        <ChevronDown class="h-[1.25rem] w-[1.25rem] shrink-0 text-[#777777] transition-transform duration-200" />
-      </slot>
-    </AccordionTrigger>
-  </AccordionHeader>
+  <AccordionTrigger
+    v-bind="delegatedProps"
+    :class="
+      cn('flex flex-1 items-center justify-center transition-all [&[data-state=open]>svg]:rotate-180', props.class)
+    ">
+    <slot />
+    <slot name="icon">
+      <ChevronDown class="h-[1.25rem] w-[1.25rem] shrink-0 text-[#777777] transition-transform duration-200" />
+    </slot>
+  </AccordionTrigger>
 </template>
 
 <script setup lang="ts">
 import { ChevronDown } from 'lucide-vue-next'
-import { AccordionHeader, AccordionTrigger, type AccordionTriggerProps } from 'radix-vue'
+import { AccordionTrigger, type AccordionTriggerProps } from 'radix-vue'
 import { computed, type HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 
